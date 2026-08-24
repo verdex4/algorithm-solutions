8a31b2161025f7736d077f680c79c2044d5dc516
Иван Ильин <noobvan338@gmail.com>
2026-08-24 22:59:42 +0500

    solved insertion sort neetcode core skills problem

--- templates/sorting/insertion_sort.py
+++ templates/sorting/insertion_sort.py
@@ -1,4 +1,4 @@
-def insertion_sort(arr: list[int]):
+def insertion_sort_base(arr: list[int]):
     n = len(arr)
 
     for i in range(1, n):
@@ -12,3 +12,34 @@ def insertion_sort(arr: list[int]):
         arr[pos] = key
 
     return arr
+
+# NEETCODE INSERTION SORT CORE SKILLS
+
+# Definition for a pair.
+class Pair:
+    def __init__(self, key: int, value: str):
+        self.key = key
+        self.value = value
+
+class Solution:
+    def insertionSort(self, pairs: list[Pair]) -> list[list[Pair]]:
+        n = len(pairs)
+        res = []
+
+        for i in range(n):
+            pos = i
+            cur = pairs[i]
+
+            while pos > 0 and pairs[pos - 1].key > cur.key:
+                pairs[pos] = pairs[pos - 1]
+                pos -= 1
+
+            pairs[pos] = cur
+            res.append(pairs.copy())
+
+        return res
+
+pairs = [Pair(3, "cat"), Pair(3, "bird"), Pair(2, "dog")]
+res = Solution().insertionSort(pairs)
+for it in res:
+    print([f"({obj.key}, {obj.value})" for obj in it], sep=" ")
