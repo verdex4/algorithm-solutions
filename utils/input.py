1f5e45b5809dfba0cb729ff1d0640ef5766d93be
Иван Ильин <noobvan338@gmail.com>
2026-09-14 14:47:03 +0500

    improved utils

--- utils/input.py
+++ utils/input.py
@@ -1,18 +1,70 @@
 import sys
+from itertools import islice
+
+
+# ДАННЫЕ БЕЗ КОЛИЧЕСТВА РАУНДОВ t
 
 # 2 числа
 data = sys.stdin.read().split()
-n, m = int(data[0]), int(data[1])
+it = iter(data)
+n, k = int(next(it)), int(next(it))
+
+# 3 числа
+data = sys.stdin.read().split()
+it = iter(data)
+n, k, m = int(next(it)), int(next(it)), int(next(it))
 
 # строка
 s = sys.stdin.read().strip()
 
 # число + список
 data = sys.stdin.read().split()
-n = int(data[0])
-arr = list(map(int, data[1:]))
+it = iter(data)
+n = int(next(it))
+arr = list(map(int, islice(it, n)))
 
-# матрица n*m
+# матрица m * n
 data = sys.stdin.read().split()
-n, m = int(data[0]), int(data[1])
-matrix = [list(map(int, data[i:i + m])) for i in range(2, 2 + n * m, m)]
+it = iter(data)
+m, n = int(next(it)), int(next(it))
+matrix = [list(map(int, islice(it, n))) for _ in range(m)]
+
+
+# ДАННЫЕ С КОЛИЧЕСТВОМ РАУНДОВ t
+
+# 2 числа
+data = sys.stdin.read().split()
+it = iter(data)
+t = int(next(it))
+for _ in range(t):
+    n, k = int(next(it)), int(next(it))
+
+# 3 числа
+data = sys.stdin.read().split()
+it = iter(data)
+t = int(next(it))
+for _ in range(t):
+    n, k, m = int(next(it)), int(next(it)), int(next(it))
+
+# строка
+data = sys.stdin.read().split()
+it = iter(data)
+t = int(next(it))
+for _ in range(t):
+    s = next(it)
+
+# число + список
+data = sys.stdin.read().split()
+it = iter(data)
+t = int(next(it))
+for _ in range(t):
+    n = int(next(it))
+    arr = list(map(int, islice(it, n)))
+
+# матрица m * n
+data = sys.stdin.read().split()
+it = iter(data)
+t = int(next(it))
+for _ in range(t):
+    m, n = int(next(it)), int(next(it))
+    matrix = [list(map(int, islice(it, n))) for _ in range(m)]
---
+++ utils/sol_template.py
@@ -0,0 +1,9 @@
+import sys
+from itertools import islice
+from collections import defaultdict, Counter, deque
+
+def solve():
+    pass
+
+if __name__ == "__main__":
+    solve()
